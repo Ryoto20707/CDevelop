b84ccb526a8a7ef3003172d5ae6166c5cfde7ccb
rakkogurumi <rakkononuigurumi@gmail.com>
2016-07-01 15:32:40 +0900

    ベースプログラム作成

---
+++ iraira.c
@@ -0,0 +1,244 @@
+#include <time.h>
+#include "map.c"
+
+int collision(); //衝突判定
+void myTimerFunc(int value); //プレイヤーを動かす
+void myKeyboardFunc(unsigned char key, int x, int y); //キーボード操作
+void mySpcialFunc(int key, int x, int y);
+void mySpcialUpFunc(int key, int x, int y);
+void display(); //glutMainloop()で繰り返し処理する関数群
+void idle();
+void init();
+void replay(); //プレイヤーを初期位置に戻す
+void timeKeeper(); //制限時間を管理する
+void gameover(); //ゲームオーバーにする
+
+
+int collision()
+{
+	//衝突判定
+	int i;
+	double MARGIN = 0.05;
+	if (z > 1)return 0;
+	for (i = 0; i < tekiIndex; i++)
+	{
+		//簡単な衝突判定
+		if ((tekiList[i][0] - x <1 - MARGIN) && (tekiList[i][0] - x >-1 + MARGIN)
+			&& (tekiList[i][1] - y <1 - MARGIN) && (tekiList[i][1] - y >-1 + MARGIN))
+		{
+			//printf("(%.02f,%.02f):(%.02f,%.02f)\n", x, y, tekiList[i][0], tekiList[i][1]);
+			gameover();
+			return 1;
+		}
+	}
+	return 0;
+}
+
+
+void myTimerFunc(int value)
+{
+	double MARGIN = 0.05;
+	int i;
+	int fric = 0;
+	for(i=0;i<fricindex;i++){
+		if(friction[i][0]<x&&x<friction[i][1]&&friction[i][2]<y&&y<friction[i][3]) fric = i;
+	}
+	int bt = 0;
+	for(i=0;i<beltindex;i++){
+		if(belt[i][0]<x&&x<belt[i][1]&&belt[i][2]<y&&y<belt[i][3]) bt = i;
+	}
+	int bt_direction = (int)belt[bt][5];
+
+	if (mySpecialValue & (1 << 0))
+	{
+		y += friction[fric][4];
+		if (collision())y -= friction[fric][4];
+		//ここを変更する
+		if (Y*L < y - MARGIN)y -= friction[fric][4];
+	}
+	if (mySpecialValue & (1 << 1))
+	{
+		x -= friction[fric][4];
+		if (collision())x += friction[fric][4];
+		//ここを変更する
+		if (0 * L > x + MARGIN)x += friction[fric][4];
+	}
+	if (mySpecialValue & (1 << 2))
+	{
+		x += friction[fric][4];
+		if (collision())x -= friction[fric][4];
+		//ここを変更する
+		if ((X - 1)*L < x - MARGIN)x -= friction[fric][4];
+	}
+	if (mySpecialValue & (1 << 3))
+	{
+		y -= friction[fric][4];
+		if (collision())y += friction[fric][4];
+		//ここを変更する
+		if (0 * L > y + MARGIN)y += friction[fric][4];
+	}
+
+	if (z > 0)
+	{
+		v -= 0.01;
+		z += v;
+		if (collision() && z< 1)
+		{
+			z -= v;
+			v = 0;
+		}
+		if (z <= 0)
+		{
+			z = 0;
+			v = 0;
+		}
+	}
+
+	if(bt_direction==1){
+		x += belt[bt][4];
+		if (collision())x -= belt[bt][4];
+		//ここを変更する
+		if ((X - 1)*L < x - MARGIN)x -= belt[bt][4];
+	}else if(bt_direction==2){
+		y += belt[bt][4];
+		if (collision())y -= belt[bt][4];
+		//ここを変更する
+		if (Y*L < y - MARGIN)y -= belt[bt][4];
+	}
+
+	//視点を移動
+	glLoadIdentity();
+	gluLookAt(0.0 + x, -10.0 + y, 2.0, 0.0 + x, 0.0 + y, 1.5, 0.0, 0.0, 1.0);
+
+	glutTimerFunc(10, myTimerFunc, 0);
+
+}
+
+void myKeyboardFunc(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case ' ':
+		//ここを変更する
+		v = 0.2;
+		z += v;
+		break;
+	}
+}
+void mySpcialFunc(int key, int x, int y)
+{
+	switch (key)
+	{
+	case GLUT_KEY_UP:
+		mySpecialValue |= 1 << 0;//mySpecialValueの1bit目を1にする
+		break;
+	case GLUT_KEY_LEFT:
+		mySpecialValue |= 1 << 1;//mySpecialValueの2bit目を1にする
+		break;
+	case GLUT_KEY_RIGHT:
+		mySpecialValue |= 1 << 2;//mySpecialValueの3bit目を1にする
+		break;
+	case GLUT_KEY_DOWN:
+		mySpecialValue |= 1 << 3;//mySpecialValueの4bit目を1にする
+		break;
+	default:
+		break;
+	}
+}
+void mySpcialUpFunc(int key, int x, int y)
+{
+	switch (key)
+	{
+	case GLUT_KEY_UP:
+		mySpecialValue &= ~(1 << 0);//mySpecialValueの1bit目を0にする
+		break;
+	case GLUT_KEY_LEFT:
+		mySpecialValue &= ~(1 << 1);//mySpecialValueの2bit目を0にする
+		break;
+	case GLUT_KEY_RIGHT:
+		mySpecialValue &= ~(1 << 2);//mySpecialValueの3bit目を0にする
+		break;
+	case GLUT_KEY_DOWN:
+		mySpecialValue &= ~(1 << 3);//mySpecialValueの4bit目を0にする
+		break;
+	default:
+		break;
+	}
+
+}
+
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glPushMatrix();
+
+	drawGround();
+	drawJiki();
+	drawTeki();
+
+	glPopMatrix();
+	glutSwapBuffers();
+	timeKeeper();
+}
+
+
+
+void idle(void)
+{
+	glutPostRedisplay();
+}
+void init(void)
+{
+	
+
+	glutKeyboardFunc(myKeyboardFunc);
+	glutSpecialFunc(mySpcialFunc);
+	glutSpecialUpFunc(mySpcialUpFunc);
+
+	glutIdleFunc(idle);
+	
+	myTimerFunc(0);
+
+	
+}
+
+clock_t start,now;
+
+void replay(){
+	x = 0.0;
+	y = 0.0;
+}
+
+void timeKeeper(){
+	int passtime;
+	now = clock();
+	passtime = now-start;
+	
+	if(passtime > 400000){
+		printf("Time Up!\n");
+		start = clock();
+		replay();
+	}
+}
+
+void gameover(){
+	printf("Game Over!\n");
+	replay();
+}
+
+
+
+int main(int argc, char *argv[])
+{	
+	start = clock();
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+	glutCreateWindow("iraira");
+	glutDisplayFunc(display);
+	mapdisplay();
+	init();
+	
+	glutMainLoop();
+	return 0;
+}
---
+++ map.c
@@ -0,0 +1,245 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <GLUT/glut.h>
+#include <math.h>
+#define PI (3.14159)
+
+
+GLfloat pos0[] = { 5.0, 0.0, 0.0, 1.0 };
+GLfloat pos1[] = { 0.0, 0.0, 5.0, 1.0 };
+enum COLOR { WHITE, RED, GREEN, BLUE, YELLOW, MAGENTA, CYAN, GRAY, BLACK };
+GLfloat color[][4] = {
+		{ 1.0, 1.0, 1.0, 1.0 },
+		{ 1.0, 0.0, 0.0, 1.0 },
+		{ 0.0, 1.0, 0.0, 1.0 },
+		{ 0.0, 0.0, 1.0, 1.0 },
+		{ 1.0, 1.0, 0.0, 1.0 },
+		{ 1.0, 0.0, 1.0, 1.0 },
+		{ 0.0, 1.0, 1.0, 1.0 },
+		{ 0.7, 0.7, 0.7, 1.0 },
+		{ 0.0, 0.0, 0.0, 1.0 } };//色を増やす場合はここに追加
+double x = 0;
+double y = 0;
+double z = 0;
+int mySpecialValue = 0;
+double tekiList[][3] = {
+		{ 0.0, 2.0, 0.0 },
+		{ 4.0, 4.0, 0.0 },
+		{ 2.0, 6.0, 0.0 },
+		{ 0.0, 10.0, 0.0 },
+		{ 4.0, 13.0, 0.0 },
+		{ 3.0, 14.0, 0.0 },
+		{ 2.0, 15.0, 0.0 },
+		{ 1.0, 16.0, 0.0 },
+		{ 3.0, 19.0, 0.0 },
+		{ 2.0, 20.0, 0.0 },
+		{ 1.0, 21.0, 0.0 },
+		{ 0.0, 22.0, 0.0 } };//障害物のリスト
+int tekiIndex = 12;
+double v = 0;
+
+int X = 5;//地面
+int Y = 25;
+double L = 1;
+
+double friction[][5] = {
+	{ 0.0, 5.0, 0.0, 25.0, 0.1}, //デフォルト値(変えないで！)
+	{ 2.0, 3.0, 15.0, 25.0, 0.05},
+	{ 4.0, 5.0, 0.0, 5.0, 0.3}
+}; //床の摩擦。x座標からx座標まで(範囲)、　y座標からy座標まで、速さ
+int fricindex = 3;
+
+double belt[][6] = {
+	{ 0.0, 5.0, 0.0, 25.0, 0.0, 0.0}, //デフォルト値(変えないで！)
+	{ 0.0, 4.0, 0.0, 3.0, 0.05, 1.0},
+	{ 2.0, 3.0, 15.0, 20.0, -0.02, 2.0}
+}; //ベルトコンベア。x座標からx座標まで(範囲)、　y座標からy座標まで、速さ、向き(+x:1,+y:2)
+int beltindex = 3;
+
+void calcNormal(GLdouble v0[3], GLdouble v1[3], GLdouble v2[3], GLdouble n[3])
+{
+	GLdouble v2v[2][3];
+	GLdouble vt[3];
+	double abs;
+	int i;
+	for (i = 0; i < 3; i++)
+	{
+		v2v[0][i] = v1[i] - v0[i];
+		v2v[1][i] = v2[i] - v0[i];
+	}
+	vt[0] = v2v[0][1] * v2v[1][2] - v2v[0][2] * v2v[1][1];
+	vt[1] = v2v[0][2] * v2v[1][0] - v2v[0][0] * v2v[1][2];
+	vt[2] = v2v[0][0] * v2v[1][1] - v2v[0][1] * v2v[1][0];
+	abs = sqrt(vt[0] * vt[0] + vt[1] * vt[1] + vt[2] * vt[2]);
+	for (i = 0; i < 3; i++)
+		n[i] = vt[i] / abs;
+}
+
+void drawGround()
+{
+	int i, j;
+	GLdouble normal[3] = { 0.0, 0.0, 1.0 };
+
+	glPushMatrix();
+
+	glNormal3dv(normal);
+
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, color[YELLOW]);
+	glMaterialfv(GL_FRONT, GL_AMBIENT, color[BLACK]);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, color[WHITE]);
+	glMaterialf(GL_FRONT, GL_SHININESS, 100.0);
+	glBegin(GL_QUADS);
+	glVertex3d(-0.5*L, -0.5*L, 0.0);//スタート
+	glVertex3d((X - 0.5)*L, -0.5*L, 0.0);
+	glVertex3d((X - 0.5)*L, 0.5*L, 0.0);
+	glVertex3d(-0.5*L, 0.5*L, 0.0);
+
+	glVertex3d(-0.5*L, (Y - 0.5)*L, 0.0);//ゴール
+	glVertex3d((X - 0.5)*L, (Y - 0.5)*L, 0.0);
+	glVertex3d((X - 0.5)*L, (Y + 0.5)*L, 0.0);
+	glVertex3d(-0.5*L, (Y + 0.5)*L, 0.0);
+	glEnd();
+
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, color[GRAY]);//灰色
+	glMaterialfv(GL_FRONT, GL_AMBIENT, color[BLACK]);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, color[WHITE]);
+	glMaterialf(GL_FRONT, GL_SHININESS, 100.0);
+	glBegin(GL_QUADS);
+	for (i = 1; i < Y; i++)
+		for (j = 0; j < X; j++)
+		{
+		GLdouble v[4][3];
+		if (!((i^j) & 1))continue;
+		v[0][0] = (j + 0 - 0.5)*L;
+		v[0][1] = (i + 0 - 0.5)*L;
+		v[0][2] = 0;
+		v[1][0] = (j + 1 - 0.5)*L;
+		v[1][1] = (i + 0 - 0.5)*L;
+		v[1][2] = 0;
+		v[2][0] = (j + 1 - 0.5)*L;
+		v[2][1] = (i + 1 - 0.5)*L;
+		v[2][2] = 0;
+		v[3][0] = (j + 0 - 0.5)*L;
+		v[3][1] = (i + 1 - 0.5)*L;
+		v[3][2] = 0;
+		glVertex3dv(v[0]);
+		glVertex3dv(v[1]);
+		glVertex3dv(v[2]);
+		glVertex3dv(v[3]);
+		}
+	glEnd();
+
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, color[BLACK]);//黒
+	glMaterialfv(GL_FRONT, GL_AMBIENT, color[BLACK]);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, color[WHITE]);
+	glMaterialf(GL_FRONT, GL_SHININESS, 100.0);
+	glBegin(GL_QUADS);
+	for (i = 0; i < Y; i++)
+		for (j = 0; j < X; j++)
+		{
+		GLdouble v[4][3];
+		if ((i^j) & 1)continue;
+		v[0][0] = (j + 0 - 0.5)*L;
+		v[0][1] = (i + 0 - 0.5)*L;
+		v[0][2] = 0;
+		v[1][0] = (j + 1 - 0.5)*L;
+		v[1][1] = (i + 0 - 0.5)*L;
+		v[1][2] = 0;
+		v[2][0] = (j + 1 - 0.5)*L;
+		v[2][1] = (i + 1 - 0.5)*L;
+		v[2][2] = 0;
+		v[3][0] = (j + 0 - 0.5)*L;
+		v[3][1] = (i + 1 - 0.5)*L;
+		v[3][2] = 0;
+		glVertex3dv(v[0]);
+		glVertex3dv(v[1]);
+		glVertex3dv(v[2]);
+		glVertex3dv(v[3]);
+		}
+	glEnd();
+
+	glPopMatrix();
+}
+
+double theta;
+void drawJiki(void)
+{
+	glPushMatrix();
+
+	glTranslatef(x, y, z);
+	glTranslatef(0.0, 0.0, 0.5);
+
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, color[GREEN]);
+	glMaterialfv(GL_FRONT, GL_AMBIENT, color[BLACK]);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, color[WHITE]);
+	glMaterialf(GL_FRONT, GL_SHININESS, 50.0);
+
+	glutSolidCube(1);
+	glPopMatrix();
+}
+void drawTeki(void)
+{
+	int i;
+	glPushMatrix();
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, color[RED]);
+	glMaterialfv(GL_FRONT, GL_AMBIENT, color[BLACK]);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, color[WHITE]);
+	glMaterialf(GL_FRONT, GL_SHININESS, 100.0);
+
+	for (i = 0; i < tekiIndex; i++)
+	{
+		glPushMatrix();
+		glTranslatef(tekiList[i][0], tekiList[i][1], tekiList[i][2]);
+		glutSolidCone(0.5, 1, 30, 30);
+		glPopMatrix();
+	}
+	glPopMatrix();
+}
+
+void mapdisplay(void)
+{
+	glClearColor(1.0, 1.0, 1.0, 0.0);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+
+	
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_LIGHT1);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(30.0, 1, .1, 100.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0.0, -10.0, 2.0, 0.0, 0.0, 1.5, 0.0, 0.0, 1.0);
+	glLightfv(GL_LIGHT1, GL_POSITION, pos1);
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, color[WHITE]);
+	
+}
+
+/*void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glPushMatrix();
+
+	drawGround();
+	drawJiki();
+	drawTeki();
+
+	glPopMatrix();
+	glutSwapBuffers();
+}
+
+int main(int argc, char *argv[])
+{
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+	glutCreateWindow("C Dev");
+	glutDisplayFunc(display);
+	mapdisplay();
+	glutMainLoop();
+	return 0;
+}*/
